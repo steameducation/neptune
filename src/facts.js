eae9b47a96af67df78bf6d474c178fbce32d6c2c
Nuno Hespanhol <nuno.hespanhol@gmail.com>
2022-07-22 15:23:52 +0100

    fix: dont include facts that overflow

--- src/facts.js
+++ src/facts.js
@@ -8,9 +8,11 @@ const facts = {
         fact: 'Mercury is the smallest planet in the Solar System measuring 4880 kms (3032 miles) wide at its equator.',
       },
       {
-        fact: 'It is impossible to live on Mercury due to its extreme temperatures: -173 °C (-279 °F) at night to 427 °C (801 °F) during the day. ',
+        fact: 'It is impossible to live on Mercury due to its extreme temperatures: -173°C (-279°F) at night to 427°C (801°F) during the day. ',
       },
-      { fact: 'A year lasts only 88 Earth days on Mercury. ' },
+      {
+        fact: 'A year lasts only 88 Earth days on Mercury. ',
+      },
       {
         fact: 'The Hubble Space Telescope cannot view Mercury because it is too close to the Sun, so the equipment would be damaged.',
       },
@@ -20,9 +22,9 @@ const facts = {
       {
         fact: 'Venus is a Roman Goddess, whose powers encompassed love, beauty, desire, sex, fertility, prosperity and victory.',
       },
-      {
-        fact: 'Venus rotates very slowly: it takes 243 Earth days to complete 1 rotation around its axis and 225 to complete its orbit around the sun. Strangely enough, a “year” lasts shorter than a “day” on Venus!',
-      },
+      // {
+      //   fact: 'Venus rotates very slowly: it takes 243 Earth days to complete 1 rotation around its axis and 225 to complete its orbit around the sun. Strangely enough, a “year” lasts shorter than a “day” on Venus!',
+      // },
       {
         fact: 'Venus is the warmest planet in our solar system, although Mercury is much closer to the sun.',
       },
@@ -44,12 +46,12 @@ const facts = {
       {
         fact: 'The Earth was once believed to be the centre of the universe.',
       },
-      {
-        fact: '70% of our planet is covered with oceans. When astronauts first went into the space, they looked back at the Earth and called our home the Blue Planet.',
-      },
-      {
-        fact: 'Each winter there are about 1 septillion (1,000,000,000,000,000,000,000,000 or a trillion trillion) snow crystals that drop from the sky.',
-      },
+      // {
+      //   fact: '70% of our planet is covered with oceans. When astronauts first went into the space, they looked back at the Earth and called our home the Blue Planet.',
+      // },
+      // {
+      //   fact: 'Each winter there are about 1 septillion (1,000,000,000,000,000,000,000,000 or a trillion trillion) snow crystals that drop from the sky.',
+      // },
     ],
 
     mars: [
@@ -57,9 +59,9 @@ const facts = {
       {
         fact: 'Mars is often referred to as the ‘Red Planet’ because of the reddish iron oxide prevalent on its surface.',
       },
-      {
-        fact: 'Mars was once believed to be home to intelligent life and – with the exception of Earth – Mars is the most hospitable to life.',
-      },
+      // {
+      //   fact: 'Mars was once believed to be home to intelligent life and, with the exception of Earth Mars, is the most hospitable to life.',
+      // },
       {
         fact: 'Mars has seasons like Earth, but they last twice as long.',
       },
@@ -73,12 +75,12 @@ const facts = {
       {
         fact: 'Jupiter is twice as big as all the other planets combined. More than 1,300 Earths would fit inside of Jupiter!',
       },
-      {
-        fact: "On Jupiter you would feel the force of gravity about 2.4 times stronger than on Earth's surface. So if you weighed 50 kg (110 pounds) here, you would weigh 120 kg (265 pounds) there!",
-      },
-      {
-        fact: "Jupiter’s Great Red Spot is actually a storm in its atmosphere. It has been raging for over 300 years and is so big that three Earth's could fit inside it!",
-      },
+      // {
+      //   fact: "On Jupiter you would feel the force of gravity about 2.4 times stronger than on Earth's surface. So if you weighed 50 kg (110 pounds) here, you would weigh 120 kg (265 pounds) there!",
+      // },
+      // {
+      //   fact: "Jupiter’s Great Red Spot is actually a storm in its atmosphere. It has been raging for over 300 years and is so big that three Earth's could fit inside it!",
+      // },
       {
         fact: 'Jupiter is composed primarily of gaseous and liquid matter. ',
       },
@@ -91,9 +93,9 @@ const facts = {
       {
         fact: 'Saturn is also known as the ‘Jewel of the Solar System’, because of its beautiful and mysterious rings and appearance.',
       },
-      {
-        fact: 'Saturn’s wind can blow up to 1800 kilometers (1118 miles) per hour, which makes it the windiest planet in our Solar System.',
-      },
+      // {
+      //   fact: 'Saturn’s wind can blow up to 1800 kilometers (1118 miles) per hour, which makes it the windiest planet in our Solar System.',
+      // },
       {
         fact: 'Saturn is so big that Earth could fit into it a whopping 755 times!',
       },
@@ -103,14 +105,14 @@ const facts = {
     ],
 
     uranus: [
+      // {
+      //   fact: 'Uranus was named after the Greek sky deity Ouranos, the earliest of the lords of the heavens.',
+      // },
+      // {
+      //   fact: 'In 1781, Uranus became the first planet discovered with the use of a telescope. At that time it was the first planet to be discovered in about 1000 years.',
+      // },
       {
-        fact: 'Uranus was named after the Greek sky deity Ouranos, the earliest of the lords of the heavens.',
-      },
-      {
-        fact: 'In 1781, Uranus became the first planet discovered with the use of a telescope. At that time it was the first planet to be discovered in about 1000 years.',
-      },
-      {
-        fact: 'Uranus is often referred to as an ‘ice giant’ planet and hits the coldest temperatures of all planets: - 224 °C (-435 °F). It has 13 rings.',
+        fact: 'Uranus is often referred to as an ‘ice giant’ planet and hits the coldest temperatures of all planets: 224°C (-435°F). It has 13 rings.',
       },
       {
         fact: 'Uranus’ poles lies West and East, equator South to North.',
@@ -125,7 +127,7 @@ const facts = {
         fact: 'Because of its blue color, Neptune is named after the Roman god of freshwater and the sea, identified with the Greek Poseidon.',
       },
       {
-        fact: 'As is the case on Uranus, it can be quite cold on Neptune, with temperatures that fall to -221 °C (-430 °F).',
+        fact: 'As is the case on Uranus, it can be quite cold on Neptune, with temperatures that fall to -221°C (-430°F).',
       },
       {
         fact: 'It takes Neptune almost 165 Earth years to circle around the sun.',
@@ -133,9 +135,9 @@ const facts = {
       {
         fact: 'The scientist Galilei was the first person who saw Neptune although he did not recognize it as a planet.',
       },
-      {
-        fact: 'Neptune has a storm similar to the Great Red Spot on Jupiter. It is commonly known as the Great Dark Spot and is roughly the size of Earth. ',
-      },
+      // {
+      //   fact: 'Neptune has a storm similar to the Great Red Spot on Jupiter. It is commonly known as the Great Dark Spot and is roughly the size of Earth. ',
+      // },
     ],
   },
   pt: {
@@ -147,7 +149,7 @@ const facts = {
         fact: 'Mercúrio é o planeta mais pequeno no sistema solar, com um perímetro de 4880 kms (3032 milhas) à volta do equador.',
       },
       {
-        fact: 'É impossível viver em Mercúrio por causa das suas temperaturas extremamente elevadas: -173 °C (-279 °F) à noite e 427 °C (801 °F) de dia. ',
+        fact: 'É impossível viver em Mercúrio por causa das suas temperaturas extremamente elevadas: -173°C (-279°F) à noite e 427°C (801°F) de dia. ',
       },
       { fact: 'Um ano dura apenas 88 dias terrestres em Mercúrio.' },
       {
@@ -159,9 +161,9 @@ const facts = {
       {
         fact: 'Vénus é uma Deusa Romana, cujos poderes incluem o amor, a beleza, o desejo, o sexo, a fertilidade, a prosperidade e a vitória.',
       },
-      {
-        fact: 'Vénus roda muito lentamente: leva 243 dias terrestres a completar uma rotação à volta do seu eixo e 225 para completar uma órbita à volta do sol. Estranhamento, em Vénus, um “ano” é mais curto que um “dia”.',
-      },
+      // {
+      //   fact: 'Vénus roda muito lentamente: leva 243 dias terrestres a completar uma rotação à volta do seu eixo e 225 para completar uma órbita à volta do sol. Estranhamento, em Vénus, um “ano” é mais curto que um “dia”.',
+      // },
       {
         fact: 'Vénus é o planeta mais quente do sistema solar, apesar de Mercúrio estar mais perto do Sol.',
       },
@@ -183,12 +185,12 @@ const facts = {
       {
         fact: 'Outrora acreditava-se que a Terra era o centro do universo.',
       },
-      {
-        fact: '70% do nosso planeta é coberto pelos oceanos. Quando os astronautas viajaram pela primeira vez ao espaço e olharam para a Terra, chamaram-lhe o Planeta Azul.',
-      },
-      {
-        fact: 'Em cada Inverno há cerca de 1 septilhão (1,000,000,000,000,000,000,000,000 ou um trilião de triliões) de cristais de neve que caem do céu.',
-      },
+      // {
+      //   fact: '70% do nosso planeta é coberto pelos oceanos. Quando os astronautas viajaram pela primeira vez ao espaço e olharam para a Terra, chamaram-lhe o Planeta Azul.',
+      // },
+      // {
+      //   fact: 'Em cada Inverno há cerca de 1 septilhão (1,000,000,000,000,000,000,000,000 ou um trilião de triliões) de cristais de neve que caem do céu.',
+      // },
     ],
 
     mars: [
@@ -198,9 +200,9 @@ const facts = {
       {
         fact: 'Marte é frequentemente chamado de “Planeta Vermelho”, por causa do óxido de ferro, avermelhado, comum na sua superfície.',
       },
-      {
-        fact: 'Outrora acreditou-se que Marte teria vida inteligente e, com a excepção da Terra, é o planeta com melhores condições para albergar a vida.',
-      },
+      // {
+      //   fact: 'Outrora acreditou-se que Marte teria vida inteligente e, com a excepção da Terra, é o planeta com melhores condições para albergar a vida.',
+      // },
       {
         fact: 'Marte tem estações, como a Terra, mas elas duram o dobro do tempo.',
       },
@@ -216,12 +218,12 @@ const facts = {
       {
         fact: 'Júpiter tem o dobro do tamanho de todos os outros combinados. Mais de 1300 Terras caberiam dentro de Júpiter.',
       },
-      {
-        fact: 'Em Júpiter sentiríamos a força da gravidade com 2.4 vezes a intensidade, quando comparada com a da superfície terrestre.',
-      },
-      {
-        fact: 'A Grande Mancha Vermelha de Júpiter é na verdade um anticiclone na sua superfície. Está em actividade há mais de 300 anos e é tão grande que caberiam três Terras no seu interior!',
-      },
+      // {
+      //   fact: 'Em Júpiter sentiríamos a força da gravidade com 2.4 vezes a intensidade, quando comparada com a da superfície terrestre.',
+      // },
+      // {
+      //   fact: 'A Grande Mancha Vermelha de Júpiter é na verdade um anticiclone na sua superfície. Está em actividade há mais de 300 anos e é tão grande que caberiam três Terras no seu interior!',
+      // },
       {
         fact: 'Júpiter é composto principalmente de matéria gasosa e líquida.',
       },
@@ -234,9 +236,9 @@ const facts = {
       {
         fact: 'Saturno é também conhecido como a “Jóia do Sistema Solar”, por causa dos seus belos e misteriosos anéis e aparência.',
       },
-      {
-        fact: 'O vento em Saturno pode soprar a velocidades de até 1800 km (1118 milhas) por hora, tornando-o no planeta mais ventoso do Sistema Solar.',
-      },
+      // {
+      //   fact: 'O vento em Saturno pode soprar a velocidades de até 1800 km (1118 milhas) por hora, tornando-o no planeta mais ventoso do Sistema Solar.',
+      // },
       {
         fact: 'Saturno é tão grande que a Terra caberia dentro dele umas espantosas 755 vezes.',
       },
@@ -249,12 +251,12 @@ const facts = {
       {
         fact: 'Urano tem o nome da divindade Grega Urano, um dos mais antigos senhores dos céus.',
       },
-      {
-        fact: 'Em 1781, Urano tornou-se no primeiro planeta descoberto com o uso de um telescópio. Nessa altura, não se descobria um novo planeta há cerca de 1000 anos.',
-      },
-      {
-        fact: 'Urano é frequentemente chamado de “Gigante Gelado”, por atingir as temperaturas mais baixas de todos os planetas: -224 ºC (-435 ºF). Tem 13 anéis.',
-      },
+      // {
+      //   fact: 'Em 1781, Urano tornou-se no primeiro planeta descoberto com o uso de um telescópio. Nessa altura, não se descobria um novo planeta há cerca de 1000 anos.',
+      // },
+      // {
+      //   fact: 'Urano é frequentemente chamado de “Gigante Gelado”, por atingir as temperaturas mais baixas de todos os planetas: -224ºC (-435ºF). Tem 13 anéis.',
+      // },
       {
         fact: 'Os pólos de Urano situam-se a Este e a Oeste, e o seu equador no eixo Norte-Sul.',
       },
@@ -268,7 +270,7 @@ const facts = {
         fact: 'Por causa da sua cor azul, Neptuno tem o nome do Deus Romano da Água e do Mar, por sua vez uma referência ao Deus Grego Posídon.',
       },
       {
-        fact: 'Como em Urano, Neptuno pode atingir temperaturas muito baixas, com temperaturas abaixo de -221 ºC (-430 ºF).',
+        fact: 'Como em Urano, Neptuno pode atingir temperaturas muito baixas, com temperaturas abaixo de -221ºC (-430ºF).',
       },
       {
         fact: 'Neptuno leva quase 165 anos terrestres para concluir uma volta ao Sol.',
@@ -276,9 +278,9 @@ const facts = {
       {
         fact: 'O cientista Galileu foi a primeira pessoa a observar Neptuno, embora não o tenha reconhecido como planeta.',
       },
-      {
-        fact: 'Neptuno tem uma tempestada parecida com a Grande Mancha Vermelha de Júpiter. É chamada de Grande Mancha Escura e tem, aproximadamente, o tamanho da Terra.',
-      },
+      // {
+      //   fact: 'Neptuno tem uma tempestada parecida com a Grande Mancha Vermelha de Júpiter. É chamada de Grande Mancha Escura e tem, aproximadamente, o tamanho da Terra.',
+      // },
     ],
   },
 }
